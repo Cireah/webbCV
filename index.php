6fa38901eff7347238e735edf2f280b3d4b8e011
simpan824 <simon.dagernas@elev.ga.ntig.se>
2022-04-27 14:44:57 +0200

    papa emeritus

--- index.php
+++ index.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>webbcv</title>
+    <title>Webb CV</title>
 </head>
 <body>
-    
+    <h1>:papasmile:</h1>
 </body>
 </html>
