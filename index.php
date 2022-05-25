1ba6d798e9ee81c2a46a3ed6faeae0746a7ef315
simpan824 <simon.dagernas@elev.ga.ntig.se>
2022-05-25 14:13:41 +0200

    yes

--- index.php
+++ index.php
@@ -10,19 +10,21 @@ session_start();
     <title>Webb CV</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<header>
-    <h1>hello</h1>
-</header>
 <body>
+<a name="home"></a>
+<div class="header">
+    <div class="headerimage">
+    </div>
+</div>
 <ul>
-    <li><a href="index.php" class="active">Home</a></li>
+    <li><a href="#home" class="active">Home</a></li>
     <li><a href="aboutme.php">About me</a></li>
     <?php if(isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true){ ?>
         <li style="float: right"><a href="logout.php">Log out</a></li>
         <li style="float: right"><a href="account.php">Account</a></li>
     <?php } else {?>
-        <li style="float: right"><a href="login.php">Sign in</a></li>
-        <li style="float: right"><a href="register.php">Sign up</a></li>
+        <li style="float: right"><a href="login.php">Log in</a></li>
+        <li style="float: right"><a href="register.php">Create Account</a></li>
     <?php } ?>
 </ul>
     <br>
@@ -32,5 +34,6 @@ session_start();
         <h1> hi </h1>
     </div>
         <?php } ?>
+        <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<h1>
 </body>
 </html>
